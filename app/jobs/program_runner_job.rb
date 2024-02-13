0b5ab83922c017505f8bc71276644e0fa297c182
Greg Matthew Crossley <greg@crossley.to>
2024-02-13 14:46:45 -0500

    Progressive fix to prev

--- app/jobs/program_runner_job.rb
+++ app/jobs/program_runner_job.rb
@@ -4,52 +4,46 @@ class ProgramRunnerJob < ApplicationJob
   queue_as :default
 
   def perform(conversation_id, most_recent_point_of_interest: nil)
-    begin
-      @conversation = Conversation.find(conversation_id)
-      @program = @conversation.program
-      @recent_remarks = @conversation.remarks.last(5)
-      @latest_location = @conversation.locations.last
-      @current_point_of_interest = "Danforth Music Halll in Toronto" #set_current_point_of_interest(most_recent_point_of_interest)
+    @conversation = Conversation.find(conversation_id)
+    @program = @conversation.program
+    @recent_remarks = @conversation.remarks.last(5)
+    @latest_location = @conversation.locations.last
+    @current_point_of_interest = set_current_point_of_interest(most_recent_point_of_interest)
 
-      # Ask the Program to generate the next remark, based on the current location and recent remarks
-      new_remark = @program
-        .build_next_remark( # build the remark
-            @current_point_of_interest, 
-            @recent_remarks,
-          )
-        .tap { |r| r.location = @latest_location } # associate the remark with the current location
-      new_remark.save! # save the remark to the database
+    # Ask the Program to generate the next remark, based on the current location and recent remarks
+    new_remark = @program
+      .build_next_remark( # build the remark
+        @current_point_of_interest,
+        @recent_remarks
+      )
+      .tap { |r| r.location = @latest_location } # associate the remark with the current location
+    new_remark.save! # save the remark to the database
 
-      new_remark_audio = new_remark
-        .generate_audio(
-          format: :mp3, 
-          encoding: :base_64_encoded
-        )
+    new_remark_audio = new_remark
+      .generate_audio(
+        format: :mp3,
+        encoding: :base_64_encoded
+      )
 
-      # send the new remark's audio to the client and prepend it to the DOM
-      @conversation.broadcast_audio(new_remark_audio) unless @conversation.reload.not_started?
-    ensure
-      # re-enqueue this job if the conversation is still active
-      ProgramRunnerJob.set(wait: 2.seconds).perform_later(conversation_id) unless @conversation.reload.not_started?
-    end
+    # send the new remark's audio to the client and prepend it to the DOM
+    @conversation.broadcast_audio(new_remark_audio) unless @conversation.reload.not_started?
+  ensure
+    # re-enqueue this job if the conversation is still active
+    ProgramRunnerJob.set(wait: 2.seconds).perform_later(conversation_id) unless @conversation.reload.not_started?
   end
 
   private
 
   def set_current_point_of_interest(most_recent_point_of_interest)
     # set the topic of conversation for the next remark
-    if most_recent_point_of_interest.nil? || @latest_location.nearby_points_of_interest.exclude?(most_recent_point_of_interest)
+    @current_point_of_interest = if most_recent_point_of_interest.nil? || @latest_location.nearby_points_of_interest.exclude?(most_recent_point_of_interest)
       # if there is no recent topic of conversation, or the client is no longer near the current topic of conversation,
       # then set the current topic of conversation to the nearest interesting thing
-      @current_point_of_interest = @latest_location.nearby_points_of_interest.first # should be the nearest thing
+      @latest_location.nearby_points_of_interest.first # should be the nearest thing
     elsif @latest_location.nearby_points_of_interest.include?(most_recent_point_of_interest)
       # if the client is still near the most recent topic of conversation, then continue
       # discussing the current topic of conversation
-      @current_point_of_interest = most_recent_point_of_interest
-    else
-      @current_point_of_interest = nil
-      # raise StandardError, "There are no interesting things nearby."
+      most_recent_point_of_interest
     end
   end
-
 end
--- app/objects/programs/base.rb
+++ app/objects/programs/base.rb
@@ -1,8 +1,15 @@
 class Programs::Base
-
   def initialize(conversation)
     raise ArgumentError, "conversation must be a Conversation" unless conversation.is_a?(Conversation)
-    @conversation = conversation 
+    @conversation = conversation
+  end
+
+  def name
+    # Assuming this method is being called from a child class
+    # (e.g. from an instance of Program::Podcast),
+    # get the name of the Program class from the class name
+    # for example, Program::Podcast => "Podcast"
+    self.class.name.demodulize
   end
 
   def role_description_for(speaker)
@@ -17,7 +24,7 @@ class Programs::Base
   def build_next_remark(current_point_of_interest, recent_remarks)
     raise ArgumentError, "current_point_of_interest must be a String" unless current_point_of_interest.is_a?(String)
     raise ArgumentError, "recent_remarks must be a collection of Remarks" unless recent_remarks.respond_to?(:all?) && recent_remarks.all? { |r| r.is_a?(Remark) }
-    
+
     # determine the next speaker (should be any speaker other than the last speaker)
     previous_speaker = recent_remarks.last&.speaker || speakers.first
     next_speaker = speakers.reject { |s| s == previous_speaker }.sample
@@ -35,5 +42,4 @@ class Programs::Base
       content: next_remark_body
     )
   end
-
 end
